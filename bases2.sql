acad0a178fbcdd10a55d7f9d68ad2251bd66b755
paulsotelo97@gmail.com <paulsotelo97@gmail.com>
2019-07-20 15:54:49 -0600

    aqui ya agrego la base

---
+++ bases2.sql
@@ -0,0 +1,81 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         5.7.24 - MySQL Community Server (GPL)
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             9.5.0.5332
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Volcando datos para la tabla proyectobases.cliente: ~0 rows (aproximadamente)
+DELETE FROM `cliente`;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+
+-- Volcando datos para la tabla proyectobases.comida: ~0 rows (aproximadamente)
+DELETE FROM `comida`;
+/*!40000 ALTER TABLE `comida` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comida` ENABLE KEYS */;
+
+-- Volcando datos para la tabla proyectobases.cuarto: ~0 rows (aproximadamente)
+DELETE FROM `cuarto`;
+/*!40000 ALTER TABLE `cuarto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cuarto` ENABLE KEYS */;
+
+-- Volcando datos para la tabla proyectobases.danos: ~0 rows (aproximadamente)
+DELETE FROM `danos`;
+/*!40000 ALTER TABLE `danos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `danos` ENABLE KEYS */;
+
+-- Volcando datos para la tabla proyectobases.factura: ~0 rows (aproximadamente)
+DELETE FROM `factura`;
+/*!40000 ALTER TABLE `factura` DISABLE KEYS */;
+/*!40000 ALTER TABLE `factura` ENABLE KEYS */;
+
+-- Volcando datos para la tabla proyectobases.inventario_ingredientes: ~0 rows (aproximadamente)
+DELETE FROM `inventario_ingredientes`;
+/*!40000 ALTER TABLE `inventario_ingredientes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `inventario_ingredientes` ENABLE KEYS */;
+
+-- Volcando datos para la tabla proyectobases.invitado: ~0 rows (aproximadamente)
+DELETE FROM `invitado`;
+/*!40000 ALTER TABLE `invitado` DISABLE KEYS */;
+/*!40000 ALTER TABLE `invitado` ENABLE KEYS */;
+
+-- Volcando datos para la tabla proyectobases.limpieza: ~0 rows (aproximadamente)
+DELETE FROM `limpieza`;
+/*!40000 ALTER TABLE `limpieza` DISABLE KEYS */;
+/*!40000 ALTER TABLE `limpieza` ENABLE KEYS */;
+
+-- Volcando datos para la tabla proyectobases.masaje: ~0 rows (aproximadamente)
+DELETE FROM `masaje`;
+/*!40000 ALTER TABLE `masaje` DISABLE KEYS */;
+/*!40000 ALTER TABLE `masaje` ENABLE KEYS */;
+
+-- Volcando datos para la tabla proyectobases.mueble: ~0 rows (aproximadamente)
+DELETE FROM `mueble`;
+/*!40000 ALTER TABLE `mueble` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mueble` ENABLE KEYS */;
+
+-- Volcando datos para la tabla proyectobases.servicios: ~0 rows (aproximadamente)
+DELETE FROM `servicios`;
+/*!40000 ALTER TABLE `servicios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `servicios` ENABLE KEYS */;
+
+-- Volcando datos para la tabla proyectobases.trabajadores: ~0 rows (aproximadamente)
+DELETE FROM `trabajadores`;
+/*!40000 ALTER TABLE `trabajadores` DISABLE KEYS */;
+/*!40000 ALTER TABLE `trabajadores` ENABLE KEYS */;
+
+-- Volcando datos para la tabla proyectobases.usuarios: ~0 rows (aproximadamente)
+DELETE FROM `usuarios`;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
