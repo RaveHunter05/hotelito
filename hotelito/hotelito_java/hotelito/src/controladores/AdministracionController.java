517495c430388017e27abd47328861550c83dac4
Francisco Paul Sotelo Rocha <paulsotelor@yahoo.es>
2019-08-11 01:04:21 -0600

    me voy a dormir que me pvtea mi gfa xd

--- hotelito/hotelito_java/hotelito/src/controladores/AdministracionController.java
+++ hotelito/hotelito_java/hotelito/src/controladores/AdministracionController.java
@@ -7,7 +7,13 @@ package controladores;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -16,6 +22,19 @@ import javafx.fxml.Initializable;
  */
 public class AdministracionController implements Initializable {
 
+    @FXML
+    private Button btnEditar;
+    @FXML
+    private ComboBox<?> cmbBusqueda;
+    @FXML
+    private TextField txtBusqueda;
+    @FXML
+    private Button btnBusqueda;
+    @FXML
+    private TableView<?> txtView;
+    
+    
+    
     /**
      * Initializes the controller class.
      */
@@ -23,5 +42,13 @@ public class AdministracionController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void btnEditar(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnBusqueda(ActionEvent event) {
+    }
     
 }
--- hotelito/hotelito_java/hotelito/src/controladores/MueblesController.java
+++ hotelito/hotelito_java/hotelito/src/controladores/MueblesController.java
@@ -7,7 +7,13 @@ package controladores;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -16,6 +22,33 @@ import javafx.fxml.Initializable;
  */
 public class MueblesController implements Initializable {
 
+    @FXML
+    private TableView<?> txtView;
+    @FXML
+    private ComboBox<?> cmbReporte;
+    @FXML
+    private TextField txtReporteNum;
+    @FXML
+    private TextField txtPrecioNum;
+    @FXML
+    private Button btnReportar;
+    @FXML
+    private ComboBox<?> cmbRegistro;
+    @FXML
+    private TextField txtReportePrecio;
+    @FXML
+    private TextField txtPrecioPrecio;
+    @FXML
+    private Button btnRegistrar;
+    @FXML
+    private Button btnEditar;
+    @FXML
+    private ComboBox<?> cmbBusqueda;
+    @FXML
+    private TextField txtBusqueda;
+    @FXML
+    private Button btnBusqueda;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +56,21 @@ public class MueblesController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void btnReportar(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnRegistrar(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnEditar(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnBusqueda(ActionEvent event) {
+    }
     
 }
